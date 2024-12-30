212023fe3c41d88ea23b5acd80da96f1f1e50bc3
matthewstephens74 <matthew.stephens@nevro.com>
2024-12-30 11:42:18 -0500

    remove unused import failing vercel lint

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -4,9 +4,6 @@ import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 
-import { put } from "@vercel/blob";
-
-
 const Blog = () => {
 	const [images, setImages] = useState([]);
 
