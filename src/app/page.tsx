771566ff0356097a4bf0b1245fdedfc88b071546
Matthew Stephens <matthewstephens@Matthews-MacBook-Pro.local>
2025-01-27 21:35:27 -0500

    add package

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
-"use-client";
-
+"use client";
 
 import Link from 'next/link';
 import { ultra } from '@/app/ui/fonts'
 import { motion } from "framer-motion";
 import FullscreenNav from "./components/FullscreenNav";
+import Spring from "./components/Spring";
 
 export default function Home() {
   return (
-    <div className="relative w-full h-screen bg-black text-white">
+    <div className="relative w-full h-screen bg-black text-white ultra">
       <FullscreenNav />
       <main className="flex items-center justify-center w-full h-screen animate-gradient-text">
         <Link href='/blog' className={`${ultra.className} text-3xl `}>Respectful Mother</Link>
@@ -16,8 +16,3 @@ export default function Home() {
     </div>
     );
 };
-
-
-//<h2 className="text-4xl font-bold">
-//          Welcome to My Creative Space 🌟
-//        </h2>
